1993d69f99dea3555dc2d439fb4684a98d97d342
xolotl <xolotl@skulldoggerous.dev>
2023-07-09 23:41:58 -0400

    Resolves #2, cleans up temporary styling

--- src/app.tsx
+++ src/app.tsx
@@ -4,14 +4,14 @@ import { Link, useRoutes, useLocation } from '@solidjs/router';
 import { routes } from './routes';
 
 const App: Component = () => {
-  
+
   const location = useLocation();
   const Route = useRoutes(routes);
-  
+
   return (
     <>
 
-      <main class="min-h-screen bg-black text-white">
+      <main class="min-h-screen  bg-black">
         <Route />
       </main>
     </>

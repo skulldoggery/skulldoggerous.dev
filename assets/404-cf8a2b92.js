b88f533d356ad1c08445625c4e88ed7d701ee24d
skulldoggery <skulldoggery@users.noreply.github.com>
2023-07-10 03:43:27 +0000

    deploy: 930f12891610ee5d136eea8548301cfb64bc7494

--- assets/404-cf8a2b92.js
+++
@@ -1,1 +0,0 @@
-import{t}from"./index-f48ebbe4.js";const o=t(`<section class="text-gray-700 p-8"><h1 class="text-2xl font-bold">404: Not Found</h1><p class="mt-4">It's gone 😞`);function e(){return o()}export{e as default};
